19248bb87ae491d5f94f164348e6a9100d4ce552
Kidsanaphon Kaeopha <itsnight134679@gmail.com>
2022-05-12 03:38:37 +0700

    Initial commit

---
+++ src/main/java/me/nightkungz/enhancedcustomblock/event/CustomBlockListener.java
@@ -0,0 +1,30 @@
+package me.nightkungz.enhancedcustomblock.event;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import me.nightkungz.enhancedcustomblock.EnhancedCustomBlock;
+import me.nightkungz.enhancedcustomblock.util.ColorUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.IOException;
+
+public class CustomBlockListener implements Listener {
+
+    private EnhancedCustomBlock plugin = EnhancedCustomBlock.getInstance();
+    private YamlDocument config = plugin.getConfiguration();
+
+    @EventHandler
+    public void onPlayerPlacesCustomBlock(BlockPlaceEvent event) throws IOException {
+
+        Player player = event.getPlayer();
+        ItemStack item = event.getItemInHand();
+
+        plugin.getConfiguration().getOptionalString("message").ifPresent(
+                (message) -> player.sendMessage(ColorUtil.color(message)));
+
+
+    }
+}
